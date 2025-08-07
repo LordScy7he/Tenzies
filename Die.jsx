ee8d9cea96f1a03523123d842c3cb0382286e14e
scy7he <pukar.pokhrel@gmail.com>
2025-08-07 13:01:49 +0545

    first commit

---
+++ Die.jsx
@@ -0,0 +1,16 @@
+export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white"
+  }
+
+  return (
+    <button
+      style={styles}
+      onClick={() => props.hold(props.id)}
+      aria-pressed={props.isHeld}
+      aria-label={`Die with value of ${props.value}, ${props.isHeld ? "held" : "not held"}`}
+    >
+      {props.value}
+    </button>
+  )
+}
